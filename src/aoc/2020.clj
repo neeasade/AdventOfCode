87b236a6a2264e67ac9ef1093acaa0eb4442580d
neeasade <neeasade@gmail.com>
2020-12-04 11:32:30 -0600

    day 4 :S

--- src/aoc/2020.clj
+++ src/aoc/2020.clj
@@ -1,4 +1,6 @@
 
+(require '[clojure.string :as string])
+(require '[clojure.math.combinatorics :as comb])
 
 (defn solve-2020-1 [input-file]
   (defn solve-comb [div]
@@ -56,4 +58,70 @@
    (apply *
           (map #(apply sled %)
                [[1 1] [3 1] [5 1] [7 1] [1 2]]))
-   ])
+   ]
+  )
+
+(defn solve-2020-4 [input-file]
+  (defn input-to-map []
+    ;; the dirtiest part of this one
+    (->> (get-input input-file)
+         ;; split by newline
+         (reduce
+          (fn [acc token]
+            (if (string/blank? token)
+              (conj acc [])
+              (vec (concat
+                    (butlast acc)
+                    [(conj (last acc)
+                           token)]))))
+          [[]])
+
+         ;; form:
+         (map flatten)
+         (map #(string/join " " %))
+         (map #(string/split % #" "))
+
+         ;; now, we have a collection of strings "key:val", so turn them into proper maps (still string-> string):
+         (map
+          (fn [coll]
+            (reduce
+             #(let [[key val] (string/split %2 #":")]
+                (assoc %1 key val))
+             {} coll)))))
+
+  (defn validate [key val]
+    (condp = key
+      "byr" (<= 1920 (Integer/parseInt val) 2002)
+      "iyr" (<= 2010 (Integer/parseInt val) 2020)
+      "eyr" (<= 2020 (Integer/parseInt val) 2030)
+      "hgt" (when-let [[_ val kind] (first (re-seq #"([0-9]+)(cm|in)" val))]
+              (if (= kind "cm")
+                (<= 150 (Integer/parseInt val) 193)
+                (<= 59 (Integer/parseInt val) 76)))
+      "hcl" (re-seq #"\#[0-9a-f]{6}" val)
+      "ecl" (#{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} val)
+      "pid" (re-seq #"^[0-9]{9}$" val)
+      "cid" true
+      nil))
+
+  (let [part-1-coll
+        (->> (input-to-map)
+             (filter
+              (fn [passport-coll]
+                (every?
+                 #((set (keys passport-coll)) %)
+                 ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"]
+                 ))))]
+    [ ;; 1
+     (count part-1-coll)
+
+     ;; 2
+     (count
+      (filter
+       (fn [passport-coll]
+         (prn passport-coll)
+         (every? #(apply validate %) passport-coll))
+       part-1-coll))
+     ]
+    )
+  )
